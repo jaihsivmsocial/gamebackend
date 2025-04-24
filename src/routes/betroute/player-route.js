44a7f6a919da3f95f0eff9dc7c2df5756efc7e4c
jaihsivmsocial <jaishiv@msocaialverse.com>
2025-04-24 12:07:39 +0530

    done

--- src/routes/betroute/player-route.js
+++ src/routes/betroute/player-route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const playerController = require("../../controller/BetController/playerController");
 const  authenticate = require("../../middleware/authMiddleware.js")
 
-router.post("/", authenticate, playerController.createPlayer);
+router.post("/name", authenticate, playerController.createPlayer);
 router.get("/get", authenticate, playerController.getPlayers);
 router.put("/:id", authenticate, playerController.updatePlayer);
 
